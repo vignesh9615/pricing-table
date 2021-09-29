01522099c6fa48415c5f02ddcb834f5d29162bcb
vignesh9615 <82431045+vignesh9615@users.noreply.github.com>
2021-09-29 13:56:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Bartab from "./components/Bartab";
+const  App = () =>{
+  return(
+  <div>
+  <Bartab />;
+  </div>)
+};
+
+export default App;
